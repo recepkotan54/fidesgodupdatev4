b49cd3d4bcc04d97fa115177d81843d7fd1e049e
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-22 20:58:53 +0300

    Update coolresim.js

--- komutlar/coolresim.js
+++ komutlar/coolresim.js
@@ -6,7 +6,7 @@ exports.run = (client, message) => {
  message.channel.send({embed: {
           "image": {
           url: coolImages.one(600, 800)},
-          color: 0xD97634,
+          color: RANDOM,
             }});};
 
 exports.conf = {
